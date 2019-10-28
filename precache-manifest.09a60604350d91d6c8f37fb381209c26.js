796ba4ed3464575db69e4227fb85fb7966b530a7
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-28 12:37:40 +0200

    Updates

---
+++ precache-manifest.09a60604350d91d6c8f37fb381209c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f0170423664eeddf89cfdf01f450fd",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "3b7fc9dd6ccd62190ad5",
+    "url": "/react_static-list-of-todos/static/css/main.d016f9b2.chunk.css"
+  },
+  {
+    "revision": "26050395c0748c903b05",
+    "url": "/react_static-list-of-todos/static/js/2.67a0c2f2.chunk.js"
+  },
+  {
+    "revision": "3b7fc9dd6ccd62190ad5",
+    "url": "/react_static-list-of-todos/static/js/main.6bd6ae25.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
